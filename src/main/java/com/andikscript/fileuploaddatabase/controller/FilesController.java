f1456c2fdb867ced7a470854e0f680ccf9f6649a
andikscript <andikscript@gmail.com>
2022-07-20 19:54:00 +0700

    add get method for show file

--- src/main/java/com/andikscript/fileuploaddatabase/controller/FilesController.java
+++ src/main/java/com/andikscript/fileuploaddatabase/controller/FilesController.java
@@ -1,18 +1,19 @@
 package com.andikscript.fileuploaddatabase.controller;
 
+import com.andikscript.fileuploaddatabase.message.ResponseFile;
 import com.andikscript.fileuploaddatabase.message.ResponseMessage;
 import com.andikscript.fileuploaddatabase.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RestController
@@ -27,5 +28,25 @@ public class FilesController {
         fileStorageService.store(file);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new ResponseMessage("Upload the file is successfull : " + file.getOriginalFilename()));
+    }
+
+    @GetMapping(value = "/files")
+    public ResponseEntity<List<ResponseFile>> getListFiles() {
+        List<ResponseFile> list = fileStorageService.getAllFiles()
+                .map(file -> {
+                    String fileDownloadUri = ServletUriComponentsBuilder
+                            .fromCurrentContextPath()
+                            .path("/files/")
+                            .path(String.valueOf(file.getId()))
+                            .toUriString();
+
+                    return new ResponseFile(
+                            file.getName(),
+                            fileDownloadUri,
+                            file.getType(),
+                            file.getData().length);
+                }).collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 }
