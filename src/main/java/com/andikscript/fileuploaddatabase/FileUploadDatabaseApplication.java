2ba02d422951a8b8e25ecce09a6ed82d2978acaa
andikscript <andikscript@gmail.com>
2022-07-20 19:17:55 +0700

    initial commit

---
+++ src/main/java/com/andikscript/fileuploaddatabase/FileUploadDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.andikscript.fileuploaddatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andikscript/fileuploaddatabase/FileUploadDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andikscript.fileuploaddatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
