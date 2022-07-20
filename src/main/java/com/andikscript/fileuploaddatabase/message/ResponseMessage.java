b36a66db99e97c12101a10b7f0513247b6cf0907
andikscript <andikscript@gmail.com>
2022-07-20 19:37:53 +0700

    add constructor

--- src/main/java/com/andikscript/fileuploaddatabase/message/ResponseMessage.java
+++ src/main/java/com/andikscript/fileuploaddatabase/message/ResponseMessage.java
@@ -4,6 +4,10 @@ public class ResponseMessage {
 
     private String message;
 
+    public ResponseMessage(String message) {
+        this.message = message;
+    }
+
     public String getMessage() {
         return message;
     }
